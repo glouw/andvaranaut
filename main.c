d0e7612525922b3bf799420725e0f22177a234c1
Gustav Louw <yellingintothefan@gmail.com>
2016-12-28 19:16:00 +0000

    texture streaming operational

--- main.c
+++ main.c
@@ -120,6 +120,10 @@ int main(void)
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* const window = SDL_CreateWindow("water", 120, 80, xres, yres, SDL_WINDOW_SHOWN);
     SDL_Renderer* const renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    const int format = SDL_GetWindowPixelFormat(window);
+    SDL_PixelFormat* info = SDL_AllocFormat(format);
+    (void)info; // Good to have, but hide for now
+    SDL_Texture* const gpu = SDL_CreateTexture(renderer, format, SDL_TEXTUREACCESS_STREAMING, xres, yres);
     // Hero init
     struct point hero = { 2.5, 3.5 };
     double theta = 0.0;
@@ -151,6 +155,9 @@ int main(void)
         const int y = temp.y;
         hero = map[y][x] ? hero : temp;
         // Buffer columns
+        void* bytes = NULL;
+        int pitch = 0;
+        SDL_LockTexture(gpu, NULL, &bytes, &pitch);
         for(int col = 0; col < xres; col++)
         {
             const double pan = 2.0 * (double)col / xres - 1.0;
@@ -170,11 +177,21 @@ int main(void)
             // Column brightness
             const double torch = 300.0;
             const double brightness = torch / (magnitude * magnitude);
-            const double lumi = brightness > 0xFF ? 0xFF : brightness;
-            //SDL_RenderDrawLine(renderer, col, top < 0.0 ? 0.0 : top, col, bot > yres ? yres : bot);
+            const unsigned lumi = brightness > 0xFF ? 0xFF : brightness;
+            // Drawing
+            #define max(a, b) (a > b ? b : a)
+            #define min(a, b) (a < b ? b : a)
+            const int a = 0;
+            const int b = min(top, 0);
+            const int c = max(bot, yres);
+            const int d = yres;
+            uint32_t* const pixel = (uint32_t*)bytes;
+            for(int j = a; j < b; j++) pixel[j * pitch / sizeof(*pixel) + col] = 0x00 << 16 | 0x00 << 8 | 0x00; // Ceiling
+            for(int j = b; j < c; j++) pixel[j * pitch / sizeof(*pixel) + col] = 0x00 << 16 | 0x00 << 8 | lumi; // Wall
+            for(int j = c; j < d; j++) pixel[j * pitch / sizeof(*pixel) + col] = 0x00 << 16 | 0x00 << 8 | 0x00; // Floor
         }
+        SDL_UnlockTexture(gpu);
         // Render columns
-        SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, gpu, NULL, NULL);
         SDL_RenderPresent(renderer);
         const int t1 = SDL_GetTicks();
@@ -185,6 +202,7 @@ int main(void)
     // Cleanup
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(gpu);
     SDL_Quit();
     return 0;
 }
