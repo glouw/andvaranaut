80625ba0efdb1d52f7783e2d2ddf4f5e8936e35c
Gustav Louw <yellingintothefan@gmail.com>
2017-04-09 08:39:11 +0000

    png sprites with colorkey

--- Surfaces.c
+++ Surfaces.c
@@ -1,14 +1,28 @@
 #include "Surfaces.h"
 #include "Util.h"
+#include <SDL2/SDL_image.h>
+#include <string.h>
 
 static SDL_Surface* load(const char* const path, const uint32_t format)
 {
-    SDL_Surface* const bmp = SDL_LoadBMP(path);
-    SDL_PixelFormat* const allocation = SDL_AllocFormat(format);
-    SDL_Surface* const converted = SDL_ConvertSurface(bmp, allocation, 0);
-    SDL_FreeFormat(allocation);
-    SDL_FreeSurface(bmp);
-    return converted;
+    const char* const extension = strchr(path, '.');
+    // Wall, ceiling, and floor bitmaps are bitmaps
+    if(strcmp(extension, ".bmp") == 0)
+    {
+        SDL_Surface* const bmp = SDL_LoadBMP(path);
+        SDL_PixelFormat* const allocation = SDL_AllocFormat(format);
+        SDL_Surface* const converted = SDL_ConvertSurface(bmp, allocation, 0);
+        SDL_FreeFormat(allocation);
+        SDL_FreeSurface(bmp);
+        return converted;
+    }
+    // Sprites are .png with a color key
+    else
+    {
+        SDL_Surface* const img = IMG_Load(path);
+        SDL_SetColorKey(img, SDL_TRUE, SDL_MapRGB(img->format, 0x00, 0xFF, 0xFF));
+        return img;
+    }
 }
 
 Surfaces pull(const char* const path, const uint32_t format)
