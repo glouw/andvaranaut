81281427a1c64a52beef3697ced9e2bf46ba7592
Gustav <yellingintothefan@gmail.com>
2017-09-04 15:06:43 -0700

    library cleaning

--- libcheck.c
+++ libcheck.c
@@ -12,6 +12,8 @@ static void sdl2()
     SDL_version l;
     SDL_VERSION(&c);
     SDL_GetVersion(&l);
+    printf("sdl2\n%d.%d.%02d\n%d.%d.%02d\n",
+        c.major, c.minor, c.patch, l.major, l.minor, l.patch);
     assert(c.major == l.major);
     assert(c.minor == l.minor);
     assert(c.patch == l.patch);
@@ -22,6 +24,8 @@ static void sdl2_ttf()
     SDL_version c;
     SDL_TTF_VERSION(&c)
     const SDL_version* l = TTF_Linked_Version();
+    printf("sdl2_ttf\n%d.%d.%02d\n%d.%d.%02d\n",
+        c.major, c.minor, c.patch, l->major, l->minor, l->patch);
     assert(c.major == l->major);
     assert(c.minor == l->minor);
     assert(c.patch == l->patch);
@@ -32,6 +36,8 @@ static void sdl2_image()
     SDL_version c;
     SDL_IMAGE_VERSION(&c);
     const SDL_version* l = IMG_Linked_Version();
+    printf("sdl2_image\n%d.%d.%02d\n%d.%d.%02d\n",
+        c.major, c.minor, c.patch, l->major, l->minor, l->patch);
     assert(c.major == l->major);
     assert(c.minor == l->minor);
     assert(c.patch == l->patch);
