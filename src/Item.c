ede1d80c55df7af856f76352ab03a7b782b05a69
Gustav Louw <yellingintothefan@gmail.com>
2018-02-26 13:26:30 -0800

    starting inventory

---
+++ src/Item.c
@@ -0,0 +1,1 @@
+#include "Item.h"
---
+++ src/Type.h
@@ -0,0 +1,31 @@
+#pragma once
+
+typedef enum
+{
+    AMMO,
+    AMULET,
+    ARMOR,
+    BOOK,
+    BOOT,
+    CHEST,
+    FLESH,
+    FOOD,
+    GLOVE,
+    HAT,
+    KEY,
+    LIGHT,
+    LONGWEP,
+    MEDWEP,
+    MONEY,
+    MUSIC,
+    POTION,
+    RING,
+    ROCK,
+    SCROLL,
+    SHIELD,
+    SHORTWEP,
+    TOOL,
+    WAND,
+    TYPES,
+}
+Type;
