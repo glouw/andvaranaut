3ab321a101415d431bc5746e010cc4e87a5a6414
Gustav Louw <yellingintothefan@gmail.com>
2018-10-05 10:56:00 -0700

    no need for pointer object in title

--- src/Title.c
+++ src/Title.c
@@ -4,21 +4,12 @@
 
 #include <stdarg.h>
 
-typedef struct
-{
-    int start;
-    int now;
-    int end;
-    int linger;
-    char* str;
-}
-Title;
-
-static Title* tt = NULL;
+// Does need to be passed around as only one title will be used.
+static Title tt;
 
 void xttadvance(const int now)
 {
-    tt->now = now;
+    tt.now = now;
 }
 
 void xttset(const int start, const int end, const int linger, const char* const fmt, ...)
@@ -32,15 +23,15 @@ void xttset(const int start, const int end, const int linger, const char* const 
 
     // Rewind and format.
     va_start(args, fmt);
-        free(tt->str);
-        tt->str = xtoss(char, len + 1);
-        vsprintf(tt->str, fmt, args);
+        free(tt.str);
+        tt.str = xtoss(char, len + 1);
+        vsprintf(tt.str, fmt, args);
     va_end(args);
 
     // If lingering the max alpha for the alpha will be used after 50% sine in/out fade.
-    tt->linger = linger;
-    tt->start = start;
-    tt->end = end;
+    tt.linger = linger;
+    tt.start = start;
+    tt.end = end;
 }
 
 void xttclear(void)
@@ -50,33 +41,32 @@ void xttclear(void)
 
 void xttstuckclear(void)
 {
-    if(tt->linger)
+    if(tt.linger)
         xttclear();
 }
 
 void xttinit(void)
 {
-    tt = xwipe(Title, 1);
     xttclear();
 }
 
 static int done(void)
 {
-    return !tt->linger && tt->now > tt->end;
+    return !tt.linger && tt.now > tt.end;
 }
 
 void xttshow(const Text text, const Sdl sdl)
 {
     if(done())
         return;
-    const float percent = (tt->now - tt->start) / (float) (tt->end - tt->start);
+    const float percent = (tt.now - tt.start) / (float) (tt.end - tt.start);
     const float max = 0xFF;
     const float alpha = max * sinf(percent * FPI);
     xfprint(
         text.fill,
         text.line,
-        tt->str,
-        (percent > 0.5f && tt->linger) ? max : alpha, // If lingering, do not fade out.
+        tt.str,
+        (percent > 0.5f && tt.linger) ? max : alpha, // If lingering, do not fade out.
         sdl.renderer,
         sdl.xres / 2,
         sdl.yres / 2);
--- src/Title.h
+++ src/Title.h
@@ -3,6 +3,16 @@
 #include "Sdl.h"
 #include "Text.h"
 
+typedef struct
+{
+    int start;
+    int now;
+    int end;
+    int linger;
+    char* str;
+}
+Title;
+
 void xttadvance(const int now);
 
 void xttset(const int start, const int end, const int linger, const char* const text, ...);
