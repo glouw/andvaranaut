64f7ccc28c604528cf2ee99923720293df008f0b
Gustav Louw <yellingintothefan@gmail.com>
2018-12-25 08:58:00 -0800

    pushing CI?

---
+++ push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+build()
+{
+    make -C src clean
+
+    OPT=-j10
+
+    for i in 00 01 10 11
+    do
+        make $OPT -C src CLANG=${i:0:1} CC=${i:1:2}
+        make $OPT -C src clean
+    done
+}
+
+push()
+{
+    git push
+}
+
+build
+push
