c429ec153749c4fcb251e89269202ff08113ce3d
Gustav Louw <airglouw@gmail.com>
2019-03-02 09:56:55 -0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+# !/bin/bash
 
-run()
+#
+# To ensure long term portability, Andvaranaut is to be compiled with all major C and C++ compilers.
+#
+# To ensure the random number generator does not break world generation,
+# Andvaranaut is to be started and exited a number of times for each compilation.
+#
+
+test()
 {
     for i in {0..25..1}
     do
@@ -12,16 +19,16 @@ run()
 
 main()
 {
+    set -e
+
     for i in 11 10 01 00
     do
         make -C src clean
 
         make -j 10 -C src CLANG=${i:0:1} CPP=${i:1:2}
 
-        run
+        test
     done
 }
-
-set -e
 
 main
